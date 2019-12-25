bb7ca7b38e0992cb02c6172afd347e96be8fa0b7
Ashah15 <ashaabdillahi53@gmail.com>
2019-12-25 21:33:50 +0300

    Modify schema.rb

--- private-events/db/schema.rb
+++ private-events/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_213920) do
+ActiveRecord::Schema.define(version: 2019_12_25_180906) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
