4988a768f382802264d5a2fe21f0c128e2cb9e4a
Marvellous Michaels <marvellousubani@gmail.com>
2020-01-07 19:13:00 +0100

    added gem-file for rspec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
 
 	def logged_in_user
       unless logged_in?
-        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
